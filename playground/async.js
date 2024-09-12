36241e7f24c831b9fa52778e05c2889ba9a3fbdc
Manuel <manuel.ma@posteo.net>
2024-09-12 22:12:01 +0200

    async challenges (1-6) made

--- playground/async.js
+++ playground/async.js
@@ -9,66 +9,105 @@ function sayHowdy() {
     console.log('Partnah');
   }
   // After thinking it through, uncomment the following line to check your guess!
-  // testMe(); // what order should these log out? Howdy or Partnah first?
+  //testMe(); // what order should these log out? Howdy or Partnah first?
   
   
   /* CHALLENGE 2 */
   
   function delayedGreet() {
-    // ADD CODE HERE
+    function greet() {
+      console.log('greet');
+    }
+    setTimeout(greet, 3000)
   }
   // Uncomment the following line to check your work!
-  // delayedGreet(); // should log (after 3 seconds): welcome
+   //delayedGreet(); // should log (after 3 seconds): welcome
   
   
   /* CHALLENGE 3 */
   
   function helloGoodbye() {
-    // ADD CODE HERE
+    function goodbye() {
+      console.log('good bye')
+    }
+    console.log('hello');
+    setTimeout(goodbye, 2000);
   }
   // Uncomment the following line to check your work!
-  // helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+  //helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
   
   
   /* CHALLENGE 4 */
   
   function brokenRecord() {
-    // ADD CODE HERE
-  }
+    function hiagain() {
+      console.log('hi again')
+    }
+    setInterval(hiagain, 1000);
+   }
   // Uncomment the following line to check your work!
-  // brokenRecord(); // should log (every second): hi again
+  //brokenRecord(); // should log (every second): hi again
   
   
   /* CHALLENGE 5 */
   
   function limitedRepeat() {
-    // ADD CODE HERE
+    let id;
+    function printHi() {
+      console.log("hi again");
+    }
+    function clear() {
+      clearInterval(id);
+    }
+    printHi();
+    id = setInterval(printHi, 1000);
+    setTimeout(clear, 2000);
   }
   // Uncomment the following line to check your work!
-  // limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+  //limitedRepeat(); // should log (every second, for 5 seconds): hi for now
   
   
   /* CHALLENGE 6 */
   
-  function everyXsecsForYsecs() {
-    // ADD CODE HERE
+  function everyXsecsForYsecs(func, interval, duration) {
+    let id;
+    function clear() {
+      clearInterval(id)
+    }
+    id = setInterval(func, interval*1000)
+    setTimeout(clear, duration*1000)
   }
   // Uncomment the following lines to check your work!
-  // function theEnd() {
-  //   console.log('This is the end!');
-  // }
-  // everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+  function theEnd() {
+     console.log('This is the end!');
+  }
+  
+  //everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
   
   
   /* CHALLENGE 7 */
   
   function delayCounter(target, wait) {
-  
+    
+    let i = 1;
+    let id;
+    
+    function showNumber() {
+      console.log(i++);
+      if (i > target) {
+        clearInterval(id);
+      }
+    }
+    function delayedShowing() {
+      id = setInterval(showNumber, wait);
+    }
+    return delayedShowing;
   }
   
+  
   // UNCOMMENT THESE TO TEST YOUR WORK!
-  // const countLogger = delayCounter(3, 1000)
-  // countLogger();
+  const countLogger = delayCounter(3, 1000)
+  countLogger();
   // After 1 second, log 1
   // After 2 seconds, log 2
   // After 3 seconds, log 3
